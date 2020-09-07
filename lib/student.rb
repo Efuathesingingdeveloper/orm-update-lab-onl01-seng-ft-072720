8343f4b052169887876d92d14c7591743cdeaf7d
Georgette Class-Peters <georgycp@udel.edu>
2020-09-07 18:09:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,3 +48,14 @@ def self.drop_table
   end 
 end
 
+def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+end
+© 2020 GitHub, Inc.
